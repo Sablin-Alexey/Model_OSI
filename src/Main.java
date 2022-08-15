eea697af585d4988bf3bdf9727dfee495a9a1fc6
Sablin-Alexey <lordsabre89@gmail.com>
2022-08-15 10:00:05 +0300

    Исправление от15.08

--- src/Main.java
+++ src/Main.java
@@ -12,21 +12,19 @@ public class Main {
 
         int port = 8089;
 
-        try (ServerSocket serverSocket = new ServerSocket(port))
-        {// порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
-                Socket clientSocket = serverSocket.accept(); // ждем подключения
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                try (
+                        Socket clientSocket = serverSocket.accept(); // ждем подключения
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.println("New connection accepted");
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
 
-                System.out.println("New connection accepted");
-
-                final String name = in.readLine();
-
-                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
             }
 
         }
-
     }
 }
